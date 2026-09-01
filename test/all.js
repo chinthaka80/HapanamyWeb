17468b2bdc3974ff35c334cea0cf4935d5b27874
chinthaka80 <nuwan755788@gmail.com>
2026-09-01 06:48:50 +0530

    STEP 23: Product Purchase & Commission Trigger Orchestrator with master idempotency, fault-tolerant notifications, and end-to-end ledger integration

--- test/all.js
+++ test/all.js
@@ -34,6 +34,7 @@ require('./step19-qualified-upline-commission.test');
 require('./step20-earnings-cap.test');
 require('./step21-wallet-ledger.test');
 require('./step22-payment-verification.test');
+require('./step23-purchase-orchestrator.test');
 
 // Run
 runTests();
